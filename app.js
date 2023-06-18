2e83efa4297133895f6807c45a10b90fa5a545d8
denizdagli <denizdaglice@gmail.com>
2023-06-18 15:52:04 +0300

    workspaces created

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+require('./src/config/dbConnection');
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World');});
